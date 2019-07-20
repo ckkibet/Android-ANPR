7a067dd999ae799c2afcd2cc8636e27ee4ac9b7c
Frank Sitedi <fsitedi@gmail.com>
2019-07-20 21:07:34 +0300

    commit from Sitedi

---
+++ sitedis/app/src/main/java/com/google/android/gms/anpr/vision/ocrreader/OcrGraphic.java
@@ -0,0 +1,120 @@
+package com.google.android.gms.anpr.vision.ocrreader;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+import com.google.android.gms.anpr.vision.ocrreader.ui.camera.GraphicOverlay;
+import com.google.android.gms.vision.text.TextBlock;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Graphic instance for rendering TextBlock position, size, and ID within an associated graphic
+ * overlay view.
+ */
+public class OcrGraphic extends GraphicOverlay.Graphic {
+
+    private static final int TEXT_COLOR = Color.WHITE;
+
+    private static Paint sRectPaint;
+    private static Paint sTextPaint;
+    private final TextBlock mText;
+    public String text;
+    Context context;
+    Intent myIntent;
+
+    OcrGraphic(GraphicOverlay overlay, TextBlock text) {
+        super(overlay);
+
+        mText = text;
+
+        if (sRectPaint == null) {
+            sRectPaint = new Paint();
+            sRectPaint.setColor(TEXT_COLOR);
+            sRectPaint.setStyle(Paint.Style.STROKE);
+            sRectPaint.setStrokeWidth(4.0f);
+        }
+
+        if (sTextPaint == null) {
+            sTextPaint = new Paint();
+            sTextPaint.setColor(TEXT_COLOR);
+            sTextPaint.setTextSize(30.0f);
+        }
+        // Redraw the overlay, as this graphic has been added.
+        postInvalidate();
+    }
+
+    public void saveContext(Context con) {
+        context = con;
+    }
+
+    /**
+     * Draws the text block annotations for position, size, and raw value on the supplied canvas.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        if (mText == null) {
+            return;
+        }
+
+        // Draws the bounding box around the TextBlock.
+        RectF rect = new RectF(mText.getBoundingBox());
+        rect.left = translateX(rect.left);
+        rect.top = translateY(rect.top);
+        rect.right = translateX(rect.right);
+        rect.bottom = translateY(rect.bottom);
+        canvas.drawRect(rect, sRectPaint);
+
+        //validation setting
+        String REGEX = "^[a-zA-z-Q-q]{1,4}\\s*[-]*[0-9]{0,2}\\s*[-]*[0-9]{3,4}$"; //regular expression
+        Pattern number; //a pattern of compiled regex
+        Matcher matcher; //helps in matching the regex
+        text = mText.getValue();
+
+        //fixing
+        Matcher m = Pattern.compile("[-][0-9]{2}[-]|[-]|[\n]").matcher(text);
+        text = m.replaceAll(" ");
+        m = Pattern.compile("ICT|Islamabad|Sindh|Punjab|PK|Govt of Sindh|Ict-Islamabad|ICT-Islamabad|KC|9|BA").matcher(text);
+        text = m.replaceAll("");
+
+        //final touch
+        text = Pattern.compile("\\s[0-9]{2}\\s").matcher(text).replaceAll(" ");
+        text = text.replaceAll("( +)", " ").trim();
+
+        //number detection
+        number = Pattern.compile(REGEX);
+        matcher = number.matcher(text);
+        if (matcher.matches()) { //print if valid
+            canvas.drawText(text, rect.centerX(), rect.bottom, sTextPaint); //draw on screen
+            Displaytext();
+            /**
+             * This displayText function is implemented in the function below that triggers the popup to be
+             * displayed and number plate
+             */
+
+//            myIntent.putExtra("result", text); //Optional parameters
+//            context.startActivity(myIntent);
+            //setting up the intent and passing data from this Ocr activity to Result Activity
+            /*
+            NOTE:   After a long time searching the web about the problem, I think the problem is with intent because this file
+                    is NOT an Activity file, the activity file is OcrCaptureActivity. There's a possibility that this might be a
+                    problem. Also, you may need to check the .xml files, I had a hard time with those.
+             */
+
+            Intent myIntent = new Intent(context,Result.class);
+            myIntent.putExtra("results",text);
+            context.startActivity(myIntent);
+
+        }
+    }
+
+    public void Displaytext() {
+        Intent intent = new Intent(context, Result.class);
+        context.startActivity(intent);
+    }
+}
---
+++ sitedis/app/src/main/java/com/google/android/gms/anpr/vision/ocrreader/Result.java
@@ -0,0 +1,170 @@
+package com.google.android.gms.anpr.vision.ocrreader;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+
+public class Result extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener{
+    Intent intent;
+    String value;
+    String text, formattedDate, formattedTime;
+    MyRecyclerViewAdapter adapter;
+    RecyclerView recyclerView;
+    private EditText input;
+    List<String> animalNames;
+    private final String KEY_RECYCLER_STATE = "recycler_state";
+    private static Bundle mBundleRecyclerViewState;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        recyclerView = findViewById(R.id.rvAnimals);
+
+        final Intent intent = getIntent();
+        text = intent.getStringExtra("results");
+
+        animalNames = new ArrayList<>();
+        animalNames.add("The list below shows a list of all vehicles scanned");
+
+        RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
+
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current time => "+c.getTime());
+        SimpleDateFormat df = new SimpleDateFormat("dd/MMMM/yyyy", Locale.getDefault());
+        SimpleDateFormat dd = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+        formattedDate = df.format(c.getTime());
+        formattedTime = dd.format(c.getTime());
+
+        final EditText input = new EditText(this);
+
+        AlertDialog.Builder a_builder = new AlertDialog.Builder(this);
+        a_builder
+                .setMessage(text
+                        +"\n"
+                        +"On: "
+                        +formattedDate
+                        +"\n"
+                        +"Time: "
+                        +formattedTime
+                        +"\n"
+                        +"Enter Driver's Name Below:"
+                        )
+                .setPositiveButton("Proceed", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        new AlertDialog.Builder(Result.this)
+                                .setMessage("Are you sure this is the correct Name: "+input.getText()+"?")
+                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Toast.makeText(Result.this, "uus" +input.getText(), Toast.LENGTH_SHORT).show();
+                                        Editable driver = input.getText();
+                                        displayResults(driver);
+                                    }
+                                })
+                                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                                .show();
+
+                    }
+                });
+
+        a_builder.setView(input);
+
+        a_builder.setNegativeButton("Scan Again", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                final Intent intent;
+                intent = new Intent(Result.this, OcrCaptureActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        AlertDialog alert = a_builder.create();
+        alert.setTitle("Scanned Plate:");
+        alert.show();
+        alert.setCancelable(false);
+    }
+
+
+    public void displayResults(Editable driver) {
+
+        String string = text
+                +"\n"
+                +"On: "
+                +formattedDate
+                +"\n"
+                +"Time: "
+                +formattedTime
+                +"\n"
+                +"Drivers name: "
+                +driver;
+        int insertIndex = 1;
+        animalNames.add(insertIndex, string);
+        adapter.notifyDataSetChanged();
+    }
+
+
+    @Override
+    public void onItemClick(View view, int position) {
+
+    }
+
+    public void onBackPressed() {
+        Intent intent = new Intent(this, OcrCaptureActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        // save RecyclerView state
+        mBundleRecyclerViewState = new Bundle();
+        Parcelable listState = Objects.requireNonNull(recyclerView.getLayoutManager()).onSaveInstanceState();
+        mBundleRecyclerViewState.putParcelable(KEY_RECYCLER_STATE, listState);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        // restore RecyclerView state
+        if (mBundleRecyclerViewState != null) {
+            Parcelable listState = mBundleRecyclerViewState.getParcelable(KEY_RECYCLER_STATE);
+            Objects.requireNonNull(recyclerView.getLayoutManager()).onRestoreInstanceState(listState);
+        }
+    }
+}
