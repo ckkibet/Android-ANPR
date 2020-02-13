7a274d400305593fc5a819a7c495357465438e99
Clement Kibet <clemkibet@gmail.com>
2020-02-13 13:25:28 +0300

    Regex edited to ensure that it supports both old and new generation of Kenyan Number Plates

--- app/src/main/java/com/google/android/gms/anpr/vision/ocrreader/OcrGraphic.java
+++ app/src/main/java/com/google/android/gms/anpr/vision/ocrreader/OcrGraphic.java
@@ -71,7 +71,7 @@ public class OcrGraphic extends GraphicOverlay.Graphic {
         canvas.drawRect(rect, sRectPaint);
 
         //validation setting
-        String REGEX = "^[a-zA-z-Q-q]{1,4}\\s*[-]*[0-9]{0,2}\\s*[-]*[0-9]{3,4}$"; //regular expression
+        String REGEX = "^[a-zA-z-Q-q]{1,4}\\s*[-]*[0-9]{0,2}\\s*[-]*[0-9]{3,4}[a-zA-z]{0,4}$"; //regular expression
         Pattern number; //a pattern of compiled regex
         Matcher matcher; //helps in matching the regex
         text = mText.getValue();
@@ -79,12 +79,10 @@ public class OcrGraphic extends GraphicOverlay.Graphic {
         //fixing
         Matcher m = Pattern.compile("[-][0-9]{2}[-]|[-]|[\n]").matcher(text);
         text = m.replaceAll(" ");
-        m = Pattern.compile("ICT|Islamabad|Sindh|Punjab|PK|Govt of Sindh|Ict-Islamabad|ICT-Islamabad|KC|9|BA").matcher(text);
-        text = m.replaceAll("");
 
         //final touch
         text = Pattern.compile("\\s[0-9]{2}\\s").matcher(text).replaceAll(" ");
-        text = text.replaceAll("( +)", " ").trim();
+        text = text.replaceAll("( +)", " ").trim().toUpperCase();
 
         //number detection
         number = Pattern.compile(REGEX);
