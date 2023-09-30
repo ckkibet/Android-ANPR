aba8ded114c5802a55259f6050308c896619da3f
Sebastian <sebestianmutahi@gmail.com>
2023-09-30 08:08:29 +0300

    commit

--- app/src/main/java/com/google/android/gms/anpr/vision/ocrreader/RecycleAdapter.java
+++ app/src/main/java/com/google/android/gms/anpr/vision/ocrreader/RecycleAdapter.java
@@ -1,12 +1,11 @@
 package com.google.android.gms.anpr.vision.ocrreader;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
